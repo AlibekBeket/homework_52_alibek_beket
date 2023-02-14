edc0114a3306373a893afcc7631f8d63402ef333
Alibek Beket <alibek1beket@gmail.com>
2023-02-14 16:06:38 +0600

    Сделал главную страницу и добавил add_view

--- app/to_do_list/urls.py
+++ app/to_do_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from to_do_list.views.to_do_list import home_view
+from to_do_list.views.to_do_list import *
 
 urlpatterns = [
-    path('', home_view)
+    path('', home_view),
+    path('to_do/add/', add_view)
 ]
--- app/to_do_list/views/to_do_list.py
+++ app/to_do_list/views/to_do_list.py
@@ -1,6 +1,15 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
+from to_do_list.models import ToDo
+
 
 def home_view(request: WSGIRequest):
-    return render(request, 'home_page.html')
+    to_do_list = ToDo.objects.all()
+    context = {
+        'to_do_list': to_do_list
+    }
+    return render(request, 'home_page.html', context=context)
+
+def add_view(request: WSGIRequest):
+    return render(request, 'add_page.html')
