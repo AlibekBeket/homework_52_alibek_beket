bcd80b1dba7247f4631a0f5f7289c39a5514dbee
Alibek Beket <alibek1beket@gmail.com>
2023-02-14 16:46:32 +0600

    Добавил функционал добваления в базу данных веденых пользователем данных

--- app/to_do_list/views/to_do_list.py
+++ app/to_do_list/views/to_do_list.py
@@ -5,6 +5,13 @@ from to_do_list.models import ToDo
 
 
 def home_view(request: WSGIRequest):
+    if request.POST:
+        to_do_add = {
+            'description': request.POST.get('description'),
+            'status': request.POST.get('status'),
+            'created_at': request.POST.get('date')
+        }
+        ToDo.objects.create(**to_do_add)
     to_do_list = ToDo.objects.all()
     context = {
         'to_do_list': to_do_list
